29a28e53b545b83920cbd608b9e69acf7567a860
yitongwuzu <33413594+yitongwuzu@users.noreply.github.com>
2019-10-11 16:01:27 +0800

    MQ

---
+++ DPC/ProjectReceive/Program.cs
@@ -0,0 +1,52 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace ProjectReceive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+            factory.HostName = "39.104.228.149";//RabbitMQ服务在本地运行
+            factory.Port = 5672;
+            factory.UserName = "guest";//用户名
+            factory.Password = "sixteam666";//密码
+
+
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    bool durable = true;
+                    channel.QueueDeclare("task_queue", durable, false, false, null);
+                    channel.BasicQos(0, 1, false);
+
+                    var consumer = new QueueingBasicConsumer(channel);
+                    channel.BasicConsume("task_queue", false, consumer);
+
+                    while (true)
+                    {
+                        var ea = (BasicDeliverEventArgs)consumer.Queue.Dequeue();
+
+                        var body = ea.Body;
+                        var message = Encoding.UTF8.GetString(body);
+
+                        int dots = message.Split('.').Length - 1;
+                        Thread.Sleep(dots * 1000);
+
+                        Console.WriteLine("Received {0}", message);
+                        Console.WriteLine("Done");
+
+                        channel.BasicAck(ea.DeliveryTag, false);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ DPC/ProjectSend/Program.cs
@@ -0,0 +1,45 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProjectSend
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //生产者
+            var factory = new ConnectionFactory();
+            factory.HostName = "39.104.228.149";//RabbitMQ服务在本地运行
+            factory.Port = 5672;
+            factory.UserName = "guest";//用户名
+            factory.Password = "sixteam666";//密码
+
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+
+                    bool durable = true;
+                    channel.QueueDeclare("task_queue", durable, false, false, null);
+
+                    string message = GetMessage(args);
+                    var properties = channel.CreateBasicProperties();
+                    properties.SetPersistent(true);
+
+
+                    var body = Encoding.UTF8.GetBytes(message);
+                    channel.BasicPublish("", "task_queue", properties, body);
+                    Console.WriteLine(" set {0}", message);
+                }
+            }
+
+        }
+        private static string GetMessage(string[] args)
+        {
+            return ((args.Length > 0) ? string.Join(" ", args) : "Hello World!");
+        }
+    }
+}
